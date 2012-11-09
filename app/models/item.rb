9a05e898bcda4a997b0b4fb999a4a1eb590741c2
Darshan Shaligram <scintilla@gmail.com>
2012-11-08 21:39:19 -0500

    Set up display.

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,47 @@ class Item
 
   # property <name>, <type>
   property :id, Serial
+
+  # The name as on the Frisco list.
   property :name, String
+
+  # If true, we will not try to look up metadata.
   property :metadata_known, Boolean
-  property :description, String
+
+  # A detailed description
+  property :description, Text
+
+  # Type of beer, flemish red, wheat, etc.
   property :item_type, String
-  property :item_stat, String
+
+  # Alcohol by volume expressed as a percentage (i.e. 7.5 for 7.5% ABV)
+  property :abv, Decimal, scale: 2
+
+  # Rating from BeerAdvocate
+  property :rating_score, Integer
+
+  property :rating_desc, String
+
+  def rating_description
+    return self.rating_desc if self.rating_desc
+    return nil unless self.rating_score
+    "BA: #{self.rating_score}"
+  end
+
+  def abv_text
+    return "? ABV" unless self.abv
+    sprintf("%.f%% ABV", self.abv)
+  end
+
+  def has_abv?
+    self.abv
+  end
+
+  def has_rating?
+    self.rating_score || self.rating_desc
+  end
+
+  def has_type?
+    self.item_type
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-names = ["Bear Republic Racer V",
-         "Brewer's Art Resurrection",
-         "Ommegang Adoration"]
+items = [
+  {
+    name: "Bear Republic Racer V",
+    item_type: 'American IPA',
+    abv: 7,
+    rating_score: 94
+  },
 
-names.find_all { |name| !Item.first(name: name) }.each { |name|
-  Item.create(name: name)
+  {
+    name: "Brewer's Art Resurrection",
+    item_type: 'Dubbel',
+    abv: 7,
+    rating_score: 87
+  },
+
+  {
+    name: "Ommegang Adoration",
+    item_type: 'Belgian Strong Dark Ale',
+    abv: 10,
+    rating_score: 90
+  }
+]
+
+items.find_all { |item| !Item.first(name: item[:name]) }.each { |item|
+  Item.create(item)
 }
