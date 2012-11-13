f8210c3182e14e28d186031f8b8bddb1afbd19a2
gsusmi <gsusmi@gmail.com>
2012-11-13 10:47:13 -0500

    Add google analytics tracking code

--- config/database.rb
+++ config/database.rb
@@ -13,16 +13,14 @@ DataMapper.logger = logger
 DataMapper::Property::String.length(255)
 DataMapper::Model.raise_on_save_failure = true
 
-STDERR.puts("ENV: #{ENV.inspect}")
-
 uri = URI.parse(ENV["REDISTOGO_URL"] || 'redis://localhost:6379')
 
 redis_config = { adapter: 'redis', host: uri.host, port: uri.port, password: uri.password }
-
-STDERR.puts("redis_config: #{redis_config.inspect} ENV: #{ENV['REDISTOGO_URL']}")
 
 case Padrino.env
   when :development then DataMapper.setup(:default, redis_config)
   when :production  then DataMapper.setup(:default, redis_config)
   when :test        then DataMapper.setup(:default, redis_config)
 end
+
+STDERR.puts("Env: #{Padrino.env}")
